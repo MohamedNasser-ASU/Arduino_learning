9e7069fc98d6cbd9eee81463e2e08a5993eceda7
Mohamed Nasser Mohamed <mohamednasser12018@gmail.com>
2026-07-16 01:51:59 +0300

    Add binary counter functionality to counter.ino
    Implement binary counting on pins 2 to 5 with delays.

--- 1.Counter/counter.ino
+++ 1.Counter/counter.ino
@@ -1,1 +1,29 @@
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+
+  // outer loop 
+  for (int i = 0; i <= 15; i++){
+    int num = i;
+    // inner loop ( converts decimal to binary)
+    for ( int j = 2; j < 6; j++){
+
+      if ( num % 2 == 0 ){
+      digitalWrite(j, LOW);
+      }
+      else digitalWrite(j, HIGH);
+      num /= 2;
+
+    }
+    delay(850);
+  }
+  
+
+
+}
 
